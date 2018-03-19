2e1a3fcd6a090808409d8df4abe4f4ccfb9d6399
FilipeAlbuquerque <filipefragosoti@gmail.com>
2018-03-19 20:49:22 -0300

    Adding DetailsActivity

--- app/src/main/java/com/arctouch/codechallenge/home/DetailsActivity.java
+++ app/src/main/java/com/arctouch/codechallenge/home/DetailsActivity.java
@@ -1,8 +1,5 @@
 package com.arctouch.codechallenge.home;
 
-/**
- * Created by filipe on 19/03/18.
- */
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
@@ -10,22 +7,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.arctouch.codechallenge.R;
 import com.bumptech.glide.Glide;
-
-/**
- * Created by filipe on 19/03/18.
- */
 
 public class DetailsActivity extends AppCompatActivity {
 
     private static final String TAG = "MovieDetailsActivity";
 
     ImageView posterImageView;
-    TextView movieTitle, overview, genres, genresIds, releaseDateTextView;
-    
-
+    TextView movieTitle, overview, releaseDateTextView;
 
     @SuppressLint("CheckResult")
     @Override
@@ -39,34 +31,32 @@ public class DetailsActivity extends AppCompatActivity {
         posterImageView.findViewById(R.id.posterImageView2);
         movieTitle.findViewById(R.id.text_title);
         overview.findViewById(R.id.text_overview);
-        genres.findViewById(R.id.text_genres);
-        genresIds.findViewById(R.id.text_genresIds);
         releaseDateTextView.findViewById(R.id.text_releaseDateTextView);
     }
 
     private void getIntentIncoming() {
 
-        if (getIntent().hasExtra("image_name") && getIntent().hasExtra("image_title")
+        if (getIntent().hasExtra("image_name") && getIntent().hasExtra("original_title")
                 && getIntent().hasExtra("image_overview") && getIntent().hasExtra("image_genres")
-                && getIntent().hasExtra("image_genresID") && getIntent().hasExtra("image_releaseDate")) {
+                && getIntent().hasExtra("image_releaseDate")) {
 
             Log.d(TAG, "getIntentIncoming: found intent extras");
 
             String imageName = getIntent().getStringExtra("image_name");
-            String textTitle = getIntent().getStringExtra("image_title");
+            String textTitle = getIntent().getStringExtra("original_title");
             String textOverview = getIntent().getStringExtra("image_overview");
-            String textGenres = getIntent().getStringExtra("image_genres");
-            String textGenresId = getIntent().getStringExtra("image_genresID");
             String textReleaseDate = getIntent().getStringExtra("image_releaseDate");
 
             //calling the method
-            setImage(textTitle, imageName, textOverview, textGenres, textGenresId, textReleaseDate);
+            setImage(textTitle, imageName, textOverview, textReleaseDate);
 
+        } else{
+            Toast.makeText(this, "NO API Data", Toast.LENGTH_SHORT).show();
         }
     }
 
     private void setImage(String textTitle, String imageName, String textOverview,
-                          String textGenres, String textGenresId, String textReleaseDate) {
+                           String textReleaseDate) {
 
         TextView title = findViewById(R.id.text_title);
         title.setText(textTitle);
@@ -74,11 +64,6 @@ public class DetailsActivity extends AppCompatActivity {
         TextView overView = findViewById(R.id.text_overview);
         overView.setText(textOverview);
 
-        TextView genres = findViewById(R.id.text_genres);
-        genres.setText(textGenres);
-
-        TextView genresID = findViewById(R.id.text_genresIds);
-        genresID.setText(textGenresId);
 
         TextView releaseDate = findViewById(R.id.text_releaseDateTextView);
         releaseDate.setText(textReleaseDate);
